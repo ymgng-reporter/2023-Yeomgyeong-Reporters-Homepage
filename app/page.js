9f29d176e6626bb88b20c73b2224b03872a78fb4
eungyolee <daaay0123@naver.com>
2023-05-09 22:13:10 +0900

    홈 화면 어느 정도 디자인 완료

--- app/page.js
+++ app/page.js
@@ -4,6 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
+  const date = new Date();
+  const today = `${date.getFullYear()}. ${
+    date.getMonth() + 1
+  }. ${date.getDate()}`;
+  const dayOfWeek = date.getDay();
+  const day = ["일", "월", "화", "수", "목", "금", "토"];
+
   return (
     <main>
       <header className="navbar">
@@ -15,8 +22,14 @@ export default function Home() {
           className="logoImage"
         />
         <h1 className="headerTitle">2023 of Yeomgyeong</h1>
-        <FontAwesomeIcon icon={faSearch} className="searchIcon" />
+        <div className="headerRight">
+          <span className="todayDate">
+            {today} ({day[dayOfWeek]})
+          </span>
+          <FontAwesomeIcon icon={faSearch} className="searchIcon" />
+        </div>
       </header>
+      <hr className="headerDivider" />
       <div className="container">
         <h1 className="reportsCategory">새 기사</h1>
         <div className="reports">
@@ -27,14 +40,36 @@ export default function Home() {
               <span className="reportHashtag">#학생기자단</span>
             </span>
             <h2 className="reportTitle">
-              염경중학교, 새로운 2023년을 맞이하다
+              염경중학교 학생기자단(구 소식지반), 동아리 개편
             </h2>
             <div className="reportInfo">
               <span className="reportDate">2023. 05. 03</span>
               <span className="reporter">이은교</span>
             </div>
           </div>
+          <div className="report">
+            <span className="reportHashtags">
+              <span className="reportHashtag">#테스트</span>
+              <span className="reportHashtag">#아</span>
+              <span className="reportHashtag">#일하기싫다</span>
+            </span>
+            <h2 className="reportTitle">test</h2>
+            <div className="reportInfo">
+              <span className="reportDate">2023. 05. 09</span>
+              <span className="reporter">이은교</span>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="footer">
+        <hr className="footerDivider" />
+        <p className="copyright">
+          Developed by{" "}
+          <b>
+            <a href="https://github.com/eungyolee">30718 이은교</a>
+          </b>
+          .<br />© 2023 염경중학교 학생기자단. All rights reserved.
+        </p>
       </div>
     </main>
   );
